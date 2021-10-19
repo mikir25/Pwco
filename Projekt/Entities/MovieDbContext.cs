fe50fad9fabbd7001319c949f8c43d5820232be4
Mikołaj Rudek <mikirudek1@op.pl>
2021-10-19 12:40:17 +0200

    Adding files

---
+++ Projekt/Entities/MovieDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt.Entities
+{
+    public class MovieDbContext : DbContext
+    {       
+        private string _connectionString =
+            "Server=DESKTOP-9CJLKPD;Database=MovieDb;Trusted_Connection=True;";
+        public DbSet<Movie> Movie { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>()
+                .Property(m => m.Title)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
